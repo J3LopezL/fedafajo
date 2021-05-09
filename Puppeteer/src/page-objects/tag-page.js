d6da819cdd40113981ebda68933fdec5586d6faa
josé López <jl.lopez77@uniandes.edu.co>
2021-05-08 21:17:01 -0500

    Funcionalidad Login y escenarios

--- Puppeteer/src/page-objects/tag-page.js
+++ Puppeteer/src/page-objects/tag-page.js
@@ -1,3 +1,15 @@
+async function createUser(page, { blogTitle, blogName, blogEmail, blogPassword }) {
+  await page.click('a[href="#/setup/two/"]');
+  await page.type('input[id="blog-title"]', blogTitle);
+  await page.type('input[id="name"]', blogName);
+  await page.type('input[id="email"]', blogEmail);
+  await page.type('input[id="password"]', blogPassword);
+  await page.click('button[type="submit"]');
+  await new Promise((r) => setTimeout(r, 2000));
+  await page.click('button[class="gh-flow-skip"]');
+  await new Promise((r) => setTimeout(r, 2000));
+}
+
 async function createTag(page, { tagName, tagSlug, tagDescription }) {
   await page.click('a[id="ember31"]');
   await page.waitForNavigation();
@@ -19,8 +31,15 @@ async function listTags(page) {
   await new Promise((r) => setTimeout(r, 2000));
 }
 
+async function homePage(page) {
+  await page.click('a[href="#/site/"]');
+  await new Promise((r) => setTimeout(r, 2000));
+}
+
 module.exports = {
+  createUser,
   createTag,
   discardChanges,
   listTags,
+  homePage,
 };
