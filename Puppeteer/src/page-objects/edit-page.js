5b0d0e2c50c81d4e7a4f178934fe6ed90d96c35f
José López <jl.lopez77@uniandes.edu.co>
2021-05-09 20:09:41 -0500

    Complemento pruebas funcionalidad 5

---
+++ Puppeteer/src/page-objects/edit-page.js
@@ -0,0 +1,21 @@
+async function listPost(page) {
+    await page.click('a[href="#/posts/"]');
+    await new Promise((r) => setTimeout(r, 2000));
+  }
+
+  async function selectPost(page) {
+    await page.click('h3.gh-content-entry-title');
+    await new Promise((r) => setTimeout(r, 2000));
+  }
+
+  async function updatePost(page) {
+    await page.click('div.gh-publishmenu.ember-view');
+    await page.click('button.gh-btn-blue');
+    await new Promise((r) => setTimeout(r, 2000));
+  }
+
+  module.exports = {
+    listPost,
+    selectPost,
+    updatePost,
+  };
