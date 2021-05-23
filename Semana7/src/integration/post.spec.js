e55059395580867ef6b415eff2f29ab45e0a0422
Fabio Bernal <f.bernal@uniandes.edu.co>
2021-05-23 19:26:31 +0200

    Modificar post

--- Semana7/src/integration/post.spec.js
+++ Semana7/src/integration/post.spec.js
@@ -1,40 +1,62 @@
 const navigationPage = require("../page-objects/navigation-page");
 const post = require("../page-objects/post-page");
 const config = require("../credentials");
-const { getTagTestData, TYPE_DATA } = require("../test-data");
+const { getPostTestData, TYPE_DATA } = require("../test-data");
 
 describe("Post", () => {
+  beforeAll(async () => {
+    // Jest config
+    jest.setTimeout(50000);
+    await navigationPage.navigate(page);
+    await navigationPage.login(page, config.user, config.password);
+  });
+
   afterEach(async () => {
     jest.setTimeout(20000);
     await navigationPage.navigate(page);
   });
 
-  it("Start section Admin", async () => {
-    await navigationPage.login(page, config.user, config.password);
-    jest.setTimeout(20000);
-    await expect(page).toMatch("Ghost Pruebas");
-  });
-});
+  describe("Random data", () => {
+    let posts = [];
+    const number = 2;
 
-describe("Post random data", () => {
-  let tags = [];
-  const number = 5;
-
-  beforeAll(async () => {
-    await navigationPage.navigate(page);
-    tags = await getTagTestData(TYPE_DATA.RANDOM, number);
-  });
-
-  for (let index = 0; index < number; index++) {
-    it(`Create Post - Random Data #${index}`, async () => {
-      const tagData = tags[index];
-      await post.listPost(page);
-      await expect(page).toMatch("Posts");
-      await post.createPost(page, tagData);
-      await post.savePost(page);
-      await post.listPost(page);
-      await expect(page).toMatch(tagData.tagName);
+    beforeAll(async () => {
+      await navigationPage.navigate(page);
+      posts = await getPostTestData(TYPE_DATA.RANDOM, number);
     });
-  }
-});
 
+    for (let index = 0; index < number; index++) {
+      it(`Create Post - Random Data #${index}`, async () => {
+        const postData = posts[index];
+        await post.listPost(page);
+        await expect(page).toMatch("Posts");
+        await post.createPost(page, postData);
+        await post.savePost(page);
+        await post.listPost(page);
+        await expect(page).toMatch(postData.titulo);
+      });
+    }
+  });
+
+  describe("A priori", () => {
+    let posts = [];
+    const number = 5;
+
+    beforeAll(async () => {
+      await navigationPage.navigate(page);
+      posts = await getPostTestData(TYPE_DATA.APRIORI, number);
+    });
+
+    for (let index = 0; index < number; index++) {
+      it(`Create Post - Random Data #${index}`, async () => {
+        const postData = posts[index];
+        await post.listPost(page);
+        await expect(page).toMatch("Posts");
+        await post.createPost(page, postData);
+        await post.savePost(page);
+        await post.listPost(page);
+        await expect(page).toMatch(postData.titulo);
+      });
+    }
+  });
+});
--- Semana7/src/page-objects/post-page.js
+++ Semana7/src/page-objects/post-page.js
@@ -3,12 +3,12 @@ async function listPost(page) {
   await new Promise((r) => setTimeout(r, 2000));
 }
 
-async function createPost(page, { tagName, tagSlug, tagDescription, tagColor }) {
+async function createPost(page, { titulo, contenido }) {
   await page.click('a[href="#/editor/post/"]');
   await page.click("textarea", { clickCount: 3 });
-  await page.type("textarea", tagName);
+  await page.type("textarea", titulo);
   await page.keyboard.press("Enter");
-  await page.type("p", tagDescription);
+  await page.type("p", contenido);
   await page.keyboard.press("Enter");
   await new Promise((r) => setTimeout(r, 2000));
 }
@@ -21,14 +21,15 @@ async function selectPost(page) {
 async function updatePost(page) {
   await page.click("div.gh-publishmenu.ember-view");
   await page.click("button.gh-btn-blue");
-  if(await page.waitForSelector('button.gh-btn-red'))
-    {await page.click('button.gh-btn-red');}
+  if (await page.waitForSelector("button.gh-btn-red")) {
+    await page.click("button.gh-btn-red");
+  }
   await new Promise((r) => setTimeout(r, 2000));
 }
 
 async function savePost(page) {
-  await page.click('div.gh-publishmenu.ember-view');
-  await page.click('button.gh-btn-blue');
+  await page.click("div.gh-publishmenu.ember-view");
+  await page.click("button.gh-btn-blue");
   await new Promise((r) => setTimeout(r, 2000));
 }
 
